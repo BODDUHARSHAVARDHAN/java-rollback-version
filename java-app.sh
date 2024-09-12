88045bfb0b4f702767dec54fb7705a26a85e8822
BODDUHARSHAVARDHAN <109265742+BODDUHARSHAVARDHAN@users.noreply.github.com>
2024-09-12 11:12:08 +0530

    Update java-app.sh

--- java-app.sh
+++ java-app.sh
@@ -17,7 +17,7 @@ Description=Java Application Service
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/java -jar ${jar_file}
+ExecStart=/usr/bin/java -jar /opt/java-app/demo-latest.jar
 User=harsha
 Restart=always
 
